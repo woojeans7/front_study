642053886307287aa6ae961531370e9488b879e5
woojeans7 <thddnwls71@naver.com>
2025-04-01 23:58:53 +0900

    07 컴포넌트 심화 기본문제 풀이

---
+++ 07_VUE/style-test/src/components/Child3.vue
@@ -0,0 +1,24 @@
+<template>
+  <div :class="$style.child">
+    <h2>Child3</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Child3',
+  created() {
+    console.log(this.$style);
+  },
+};
+</script>
+
+<style module>
+/* CSS를 객체처럼 다룰 수 있게 함. */
+.child {
+  background-color: orange;
+  border: solid 1px black;
+  margin: 1.5em;
+  padding: 1em;
+}
+</style>
